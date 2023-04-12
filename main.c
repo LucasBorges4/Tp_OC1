b3e97c238aa2c1f55e44176458d54e51b80240aa
LucasBorges4 <Lucasborges-2013@hotmail.com>
2023-04-11 22:28:00 -0300

    Correção da função decimal_to_binary

--- main.c
+++ main.c
@@ -1,12 +1,9 @@
 #include "montador.h"
+#include <stdio.h>
 
 int main(void){
 
-
-
-
-
-
+    printf("%d\n", decimal_to_binary(30));
 
     return 0;
 }
--- montador.c
+++ montador.c
@@ -4,7 +4,7 @@
 int decimal_to_binary(int decimal){
     int i = 0, resultado = 0;
     while (decimal != 0) {
-        resultado = decimal%2 * pow(10, i);
+        resultado += decimal%2 * pow(10, i);
         decimal/=2;
         i++;
     }
