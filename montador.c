bb7e198bff1d77e68d37e548b1212e52310cc183
LucasBorges4 <Lucasborges-2013@hotmail.com>
2023-04-11 22:18:22 -0300

    11/04/2023 - Adicionado : decimal_to_binary

--- montador.c
+++ montador.c
@@ -1,4 +1,13 @@
 #include "montador.h"
+#include <math.h>
 
-
+int decimal_to_binary(int decimal){
+    int i = 0, resultado = 0;
+    while (decimal != 0) {
+        resultado = decimal%2 * pow(10, i);
+        decimal/=2;
+        i++;
+    }
+    return resultado;
+}
 
--- montador.h
+++ montador.h
@@ -1,10 +1,11 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <math.h>
 
 
 char concatenar_string(char* str_A, char* str_B);
 char* instruct_Converter_to_Binary(char* str);
-
+int decimal_to_binary(int decimal);
 
 
