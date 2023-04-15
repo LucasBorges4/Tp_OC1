f246518e95dd8ae9ea996b9f182c950f8e2ed39a
LucasBorges4 <Lucasborges-2013@hotmail.com>
2023-04-15 16:08:02 -0300

    16:04 - 15/04/2023 - Commit

--- program/main.c
+++ program/main.c
@@ -7,32 +7,17 @@ int main(void){
     FILE *arq_Entrada;
     FILE *arq_Saida;
 
-    Type_I* instrucoes_tipo_I;
-    Type_R* instrucoes_tipo_R;
-    Type_S* instrucoes_tipo_S;
-
     Type_I* vetor_I;
     Type_R* vetor_R;
     Type_S* vetor_S;
 
-<<<<<<< HEAD
     // Tipo I addi, andi, ori, lw, lb, lh, -> 6 
     // Tipo R add, sub, and, or, xor, sll e srl -> 7
     // Tipo S sw, sb e sh -> 3
-=======
-    // Tipo I addi, andi, ori, lw, lb, lh -> 6
-    // Tipo R add, sub, and, or, xor, sll e srl -> 7
-    // Tipo S sw, sb e sh -> 3
->>>>>>> refs/remotes/origin/main
     
     vetor_I = (Type_I*) malloc(6*sizeof(Type_I));
     vetor_R = (Type_R*) malloc(7*sizeof(Type_R));
     vetor_S = (Type_S*) malloc(3*sizeof(Type_S));
-
-
-
-    instrucoes_tipo_I->nome_Instrucao = *string;
-    printf("%s\n", &instrucoes_tipo_I->nome_Instrucao[0]);
 
     char* endereco_entrada;
 
