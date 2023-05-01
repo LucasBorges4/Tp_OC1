1002bc895a141212d100d5e10970d9e3b7620249
LucasBorges4 <Lucasborges-2013@hotmail.com>
2023-05-01 11:16:40 -0300

    MUDANÇAS DE QUALIDADE

--- program/main.c
+++ program/main.c
@@ -27,32 +27,38 @@ int main(void){
     char instrucoes_B[NUM_INSTRUCT_TYPE_B][4] = {"beq", "bne", "blt", "bge"};
     // beq OPCODE = 1100011 FUNC 3 = 000
                                     
-    int instrucoes_I_codes[6][2] = {{0,10011},{111,10011},{110,10011},{10,11},{0,11},{1,11}}; //func3,opcode7
-    int instrucoes_R_codes[7][3] = {{0,0,110011},{100000,0,110011},{0,111,110011},{0,110,110011},{0,100,110011},{0,1,110011},{0,101,110011}}; //func7,func3,opcode
-    int instrucoes_S_codes[3][2] = {{10,100011},{0,100011},{1,100011}}; //func3,opcode7
-    int instrucoes_B_codes[4][2] = {{000,1100011}, {001, 1100011}, {100, 1100011}, {101, 1100011}}; //func3, opcode7
+    int instrucoes_I_codes[NUM_INSTRUCT_TYPE_I][2] = {{0,10011},{111,10011},{110,10011},{10,11},{0,11},{1,11}}; //func3,opcode7
+    int instrucoes_R_codes[NUM_INSTRUCT_TYPE_R][3] = {{0,0,110011},{100000,0,110011},{0,111,110011},{0,110,110011},{0,100,110011},{0,1,110011},{0,101,110011}}; //func7,func3,opcode
+    int instrucoes_S_codes[NUM_INSTRUCT_TYPE_S][2] = {{10,100011},{0,100011},{1,100011}}; //func3,opcode7
+    int instrucoes_B_codes[NUM_INSTRUCT_TYPE_B][2] = {{000,1100011}, {001, 1100011}, {100, 1100011}, {101, 1100011}}; //func3, opcode7
     
     char *p;
 
     int cont = 0;
     
-    for(int x = 0; x < 6; x++){ //Loop for para adicionar func3 e opcode para cada instrucao do tipo I
+    for(int x = 0; x < NUM_INSTRUCT_TYPE_I; x++){ //Loop for para adicionar func3 e opcode para cada instrucao do tipo I
         strcpy(vetor_I[x].nome_Instrucao,instrucoes_I[x]); //Passa o nome contido em instrucoes_I no indice x para a instrucao I no indice x
         vetor_I[x].funct_3 = instrucoes_I_codes[x][0]; //Passa o func3 contido em instrucoes_I_codes no indice {x,0} para a instrucao I no indice x
         vetor_I[x].opcode_7 = instrucoes_I_codes[x][1]; //Passa o opcode contido em instrucoes_I_codes no indice {x,1} para a instrucao I no indice x
     }
     
-    for(int x = 0; x < 7; x++){ //Loop for para adicionar func7,func3 e opcode para cada instrucao do tipo R
+    for(int x = 0; x < NUM_INSTRUCT_TYPE_R; x++){ //Loop for para adicionar func7,func3 e opcode para cada instrucao do tipo R
         strcpy(vetor_R[x].nome_Instrucao,instrucoes_R[x]); //Passa o nome contido em instrucoes_R no indice x para a instrucao R no indice x
         vetor_R[x].funct_7 = instrucoes_R_codes[x][0]; //Passa o func7 contido em instrucoes_R_codes no indice {x,0} para a instrucao R no indice x
         vetor_R[x].funct_3 = instrucoes_R_codes[x][1]; //Passa o func3 contido em instrucoes_R_codes no indice {x,1} para a instrucao R no indice x
         vetor_R[x].opcode_7 = instrucoes_R_codes[x][2]; //Passa o opcode contido em instrucoes_R_codes no indice {x,2} para a instrucao R no indice x
     }
 
-    for(int x = 0; x < 3; x++){ //Loop for para adicionar func3 e opcode para cada instrucao do tipo S
+    for(int x = 0; x < NUM_INSTRUCT_TYPE_S; x++){ //Loop for para adicionar func3 e opcode para cada instrucao do tipo S
         strcpy(vetor_S[x].nome_Instrucao,instrucoes_S[x]); //Passa o nome contido em instrucoes_S no indice x para a instrucao S no indice x
         vetor_S[x].funct_3 = instrucoes_S_codes[x][0]; //Passa o func3 contido em instrucoes_S_codes no indice {x,0} para a instrucao S no indice x
         vetor_S[x].opcode_7 = instrucoes_S_codes[x][1];  //Passa o opcode contido em instrucoes_S_codes no indice {x,1} para a instrucao S no indice x
+    }
+
+    for(int x = 0; x < NUM_INSTRUCT_TYPE_B; x++){ //Loop for para adicionar func3 e opcode para cada instrucao do tipo B
+        strcpy(vetor_B[x].nome_Instrucao,instrucoes_B[x]); //Passa o nome contido em instrucoes_B no indice x para a instrucao B no indice x
+        vetor_B[x].funct_3 = instrucoes_B_codes[x][0]; //Passa o func3 contido em instrucoes_B_codes no indice {x,0} para a instrucao B no indice x
+        vetor_B[x].opcode_7 = instrucoes_B_codes[x][1]; //Passa o opcode contido em instrucoes_B_codes no indice {x,1} para a instrucao B no indice x
     }
     
     char* endereco_entrada; //String para armazenar o endereco de entrada
@@ -64,10 +70,6 @@ int main(void){
    // decimal_to_Binary_neg("-2");
     char* str = "add";
 
-    printf("%d\n", le_instrucao_R(vetor_R, str));
-    printf("%d\n", le_instrucao_I(vetor_I, str));
-    printf("%d\n", le_instrucao_S(vetor_S, str));
-    
     Type_I binary_I;
     Type_R binary_R;
     Type_S binary_S;
