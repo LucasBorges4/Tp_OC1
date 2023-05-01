1bb98657bc826297e98063429223f2cac5b2d714
LucasBorges4 <Lucasborges-2013@hotmail.com>
2023-05-01 19:36:30 -0300

    REMAKE

--- program/montador.c
+++ program/montador.c
@@ -1,19 +1,14 @@
 #include "montador.h"
-
-
 size_t decimal_to_Binary(size_t decimal){ //Funcao que objetiva receber um decimal e retornar um binario de tipo size_t(similar ao unsigned int)
     size_t resultado = 0; //Variavel para armazenar o resultado final(binario)
     size_t fator = 1;
-
     while (decimal != 0) {  //Loop termina quando o decimal for zero
         resultado += decimal%2 * fator; //Resultado recebe ele mesmo mais o resto da variavel decimal por 2 vezes a potencia 10^i
         fator *= 10;
         decimal/=2; //Decimal reduzido na metade
         }   
-
     return resultado;
 }
-
 size_t octal_to_binary(size_t octal){ //Funcao que objetiva receber um octal e retornar um binario de tipo size_t(similar ao unsigned int)
     size_t resultado = 0; //Variavel para armazenar o resultado final(binario)
     u_short i = 0;
@@ -26,7 +21,6 @@ size_t octal_to_binary(size_t octal){ //Funcao que objetiva receber um octal e r
     resultado = decimal_to_Binary(resultado); //Resultado, agora em decimal, e transformado em binario pela funcao decimal_to_Binary
     return resultado;
 }
-
 size_t hexa_to_binary(char* hexa){  //Funcao que objetiva receber um hexadecimal e retornar um binario de tipo size_t(similar ao unsigned int)
     size_t resultado = 0;  //Variavel para armazenar o resultado final(binario)
     u_short i = 0, temp, TAM;
@@ -37,7 +31,6 @@ size_t hexa_to_binary(char* hexa){  //Funcao que objetiva receber um hexadecimal
             temp = hexa[i] - 48; //Variavel temp recebe o char no indice i da string hexa convertido para inteiro('0' -> 0, '1'->1)
             resultado += temp * pow(16, TAM - i - 1); //O resultado incrementa em temp vezes 16^(comprimento da string hexa - indice - 1)
         }
-
         else if (hexa[i] >= 97 && hexa[i] <= 102) { //Analizamos o char no indice i da string hexa e vemos se ele esta entre 'a' e 'f' na tabela Ascii (a->97 ate f->102)
             temp = hexa[i] - 87;//Variavel temp recebe o char no indice i da string hexa convertido para inteiro('a' -> 97, 'b'->98)
             resultado += temp * pow(16, TAM - i - 1); //O resultado incrementa em temp vezes 16^(comprimento da string hexa - indice - 1)
@@ -48,18 +41,9 @@ size_t hexa_to_binary(char* hexa){  //Funcao que objetiva receber um hexadecimal
     resultado = decimal_to_Binary(resultado); //O resultado, agora em decimal, e transformado em binario pela funcao decimal_to_Binary
     return resultado;
 }
-
-
 int shift_Num(int num){ //Funcao com o objetivo de realizar uma operacao de deslocamento de bits a esquerda
     return num << 1; //retorna o numero com o deslocamento feito
 }
-
-void limpa_Arquivo(FILE* arq, const char* str){
-    arq = fopen(str, "w");
-    fprintf(arq, ""); //LIMPA ARQUIVO DE SAIDA;
-    fclose(arq);
-}
-
 void abre_Arquivo(FILE** arqEntrada,FILE** arqSaida, char* endereco_Entrada){ //Funcao com o objetivo de abrir os possiveis arquivos de entrada e saida
     
     *arqEntrada = fopen(endereco_Entrada,"r"); //Abre o arquivo de entrada no modo 'leitura'
@@ -69,7 +53,6 @@ void abre_Arquivo(FILE** arqEntrada,FILE** arqSaida, char* endereco_Entrada){ //
         exit(1); 
     }
 }
-
 short le_instrucao_I(Type_I* montador, char* str){ //Retorna o indice da instrucao no tipo I, se existir, caso contrario, retorna -1
     for (int i = 0; i < 6; i++) {
         if (!strcmp(montador[i].nome_Instrucao, &str[0])) {
@@ -78,17 +61,14 @@ short le_instrucao_I(Type_I* montador, char* str){ //Retorna o indice da instruc
     }
     return -1;
 }
-
 short le_instrucao_R(Type_R *montador, char* str){ //Retorna o indice da instrucao no tipo R, se existir, caso contrario, retorna -1
     for (int i = 0; i < 7; i++) {
         if (!strcmp(montador[i].nome_Instrucao, &str[0])) {
             return i;
         }
     }
-
     return -1;
 }
-
 short le_instrucao_S(Type_S *montador, char* str){ //Retorna o indice da instrucao no tipo S, se existir, caso contrario, retorna -1
     for (int i = 0; i < 3; i++) {
         if (!strcmp(montador[i].nome_Instrucao, &str[0])) {
@@ -97,7 +77,6 @@ short le_instrucao_S(Type_S *montador, char* str){ //Retorna o indice da instruc
     }
     return -1;
 }
-
 short le_instrucao_B(Type_B *montador, char* str){ //Retorna o indice da instrucao no tipo B, se existir, caso contrario, retorna -1
     for (int i = 0; i < 3; i++) {
         if (!strcmp(montador[i].nome_Instrucao, &str[0])) {
@@ -106,7 +85,6 @@ short le_instrucao_B(Type_B *montador, char* str){ //Retorna o indice da instruc
     }
     return -1;
 }
-
 short pesquisa_instrução(char* str, Type_I* montador_I, Type_R* montador_R, Type_S* montador_S, Type_B *montador_B,
 Type_I * Result_I,Type_R * Result_R,Type_S * Result_S,Type_B * Result_B){ //Funcao para encontrar o tipo de uma certa instrucao e atribuir ao devido struct seus parametros(funct_3,funct_7,opcode)
         
@@ -123,12 +101,10 @@ Type_I * Result_I,Type_R * Result_R,Type_S * Result_S,Type_B * Result_B){ //Func
        int index = le_instrucao_S(&montador_S[i], str); //Indice recebe o possivel indice da instrucao no vetor S
         if (index != -1){  //Caso nao for -1, a funcao pertence nesse tipo
             *Result_S = montador_S[index];  //Result_S recebe a struct no indice index no montador_S
-
             return 2; //Retorna 2 se instrucao for do tipo S
             break;
         }
     }
-
     for (int i = 0; i < NUM_INSTRUCT_TYPE_R; i++) {
         int index = le_instrucao_R(&montador_R[i], str); //Indice recebe o possivel indice da instrucao no vetor R
         if (index != -1){     //Caso nao for -1, a funcao pertence nesse tipo
@@ -145,19 +121,15 @@ Type_I * Result_I,Type_R * Result_R,Type_S * Result_S,Type_B * Result_B){ //Func
             break;
         }
     }
-
     return 0; //Retorna 0 se a instrucao for desconhecida    
 }
 /*Funções de leitura de arquivo*/
-
 short le_Linha(FILE *arqEntrada, Type_I* vetor_I, Type_R* vetor_R, Type_S* vetor_S, Type_B* vetor_B, Type_I *Result_I, Type_R *Result_R, Type_S *Result_S, Type_B *Result_B){ //Funcao com o objetivo de ler o arquivo de entrada linha por linha
     //Declara a string para a linha, aloca e armazena a linha
     char *linha; //String para armazenar a linha atual
     short TAM_linha = 100; //Limite de caracteres por linha
-
     linha = (char*) malloc(TAM_linha*sizeof(char)); //Aloca memoria para a string linha baseado no limite de caracteres por linha
     fgets(linha, TAM_linha, arqEntrada); //Funcao para passar para a string linha uma linha do arquivo de entrada
-
     //Mede o tamanho da 'string' lida no arquivo;
     TAM_linha = strlen(linha); 
     
@@ -170,19 +142,12 @@ short le_Linha(FILE *arqEntrada, Type_I* vetor_I, Type_R* vetor_R, Type_S* vetor
         return 0;
     }
     //Lê a linha;
-
     const char delim[3] = " ("; //Delimitador a ser utilizado
     char *token;
-
     token = strtok(linha, delim);  //A funcao strtok e responsavel por separar a string baseada em certos delimitadores, que nesse caso e um espaco
-
     short i = 0; //variavel indice
-
     char str[4][5]; //string constante que armazenara o vetor de strings gerado pela separacao da linha baseado em espacos
-
     int num, num1, num2;
-    int negative = 0;
-
     while (token != NULL) { //Enquanto token ainda nao for nulo
         strcpy(str[i], token); //Atribuido ao vetor str no indice i o valor de token
         token = strtok(NULL, delim); //token recebe um strtok para que seja separado novamente pelo delimitador espaco
@@ -192,21 +157,16 @@ short le_Linha(FILE *arqEntrada, Type_I* vetor_I, Type_R* vetor_R, Type_S* vetor
     //printf("%s %s %s %s\n", str[0], str[1], str[2], str[3]);
     
     sscanf(str[1], "%*[^0123456789]%d", &num); //Procura no primeiro parametro algum numero e atribui a num
-
     if (str[2][0] == 120) {
-        
         sscanf(str[2], "%*[^0123456789]%d", &num1); //Procura no segundo paramentro algum numero e atribui a num1
     } // se caso for um registrador já que x = 120 na tabela ascii, entao passa pelo if e faz a procura
-
     else {
         sscanf(str[2], "%d", &num1); //caso nao, entao é um imediato, se é entao procura e converte-lo para inteiro
     }
-
     if (str[3][0] == 120) {
         sscanf(str[3], "%*[^0123456789]%d", &num2);  //Procura no segundo paramentro algum numero e atribui a num1
         // se caso for um registrador já que x = 120 na tabela ascii, entao passa pelo if e faz a procura
     }
-
     else {
         sscanf(str[3], "%d", &num2); //caso nao, entao é um imediato, se é entao procura e converte-lo para inteiro
     }
@@ -215,92 +175,53 @@ short le_Linha(FILE *arqEntrada, Type_I* vetor_I, Type_R* vetor_R, Type_S* vetor
         
     int entrada = 0;
     FILE *fp;
+    fp = fopen("./stdin/binary.txt", "a+");
+
     
     if (tipo == 1) {
         set_registradores_I(Result_I, num,  num2, num1);
         //referenciado de acordo com comando da entrada.
         if (entrada) printf("%s\n", get_I_binary(*Result_I));
-        else 
+        else {
             fprintf(fp, "%s\n", get_I_binary(*Result_I));
-        
-    }
-
-    if (tipo == 2) {
-        if (num1 < 0) {
-            
         }
+    }
+    if (tipo == 2) {
         set_registradores_S(Result_S, num1, num, num2);
         if (entrada) printf("%s\n", get_S_binary(*Result_S)); //referenciado de acordo com comando da entrada
         else fprintf(fp, "%s\n", get_S_binary(*Result_S));
     }
-
     if (tipo == 3) {
         set_registradores_R(Result_R, num, num1, num2);
         if (entrada) printf("%s\n", get_R_binary(*Result_R)); //refernciado de acordo com ocmando da entrada.
         else fprintf(fp, "%s\n", get_R_binary(*Result_R));
     }
-
     if (tipo == 4) {
-
         set_registradores_B(Result_B, num2, num, num1);
         if (entrada) printf("%s\n", get_B_binary(*Result_B)); //refernciado de acordo com ocmando da entrada.
         else fprintf(fp, "%s\n", get_B_binary(*Result_B));
     }
     
-
     //Libera memoria e retorna;
+    
     fclose(fp);
     free(linha);
     return 1;
-
 }
 
-void char_To_Binary(char c, char *binario) {
-    for (int i = 0; i < 12; i++) {
-        binario[11-i] = (c & (1 << i)) ? '1' : '0';
-    }
-    binario[12] = '\0';
-}
-
-void invert_Binary(char *binario) {
-    for (int i = 0; i < 12; i++) {
-        binario[i] = (binario[i] == '1') ? '0' : '1';
-    }
-}
-
-char* convert_Neg_To_Bin(char *str) {
-    // alocar espaço para o binário e o resultado
-    char *binario = (char *)malloc(13 * sizeof(char));
-    char *resultado = (char *)malloc(13 * sizeof(char));
-
-    // converter a string para binário
-    char c = strtol(str, NULL, 10);
-    char_To_Binary(c, binario);
-
-    // inverter os 1's e 0's do binário
-    invert_Binary(binario);
-
-    // adicionar 1 ao binário invertido
-    int carry = 1;
-    for (int i = 11; i >= 0; i--) {
-        int soma = (binario[i] - '0') + carry;
-        resultado[i] = (soma % 2) + '0';
-        carry = soma / 2;
-    }
-    resultado[12] = '\0';
-    free(binario);
-    return resultado;
+void limpa_Arquivo(FILE* arq, const char* str){
+    arq = fopen(str, "w");
+    fprintf(arq, ""); //LIMPA ARQUIVO DE SAIDA;
+    fclose(arq);
 }
 
 char* adicionar_zeros_esquerda(int binario, int num_bits){ //Funcao com o objetivo de adicionar zeros a esquerda baseado em um numero de bits especifico
-    
+
     char *str = (char*)malloc(sizeof(char)*num_bits); //str alocada dinamicamente com num_bits caracteres
     sprintf(str, "%0*d", num_bits,binario); //sprintf para que sejam adicionados zeros a esquerda ate que o binario tenha num_bits bits
     
     return str; //Retorna o binario, que, agora, e representado por uma string, visto que zeros a esquerda sao removidos por padrao em tipos inteiros
 }
-
-
 char *get_R_binary(Type_R struct_R){ //Funcao que retorna o binario de 32 bits do tipo R
     char *binary=(char*)malloc(sizeof(char)*32); //Alocacao de 32 chars
     /*Passa todos os inteiros contidos na estrutura R em
@@ -313,7 +234,6 @@ char *get_R_binary(Type_R struct_R){ //Funcao que retorna o binario de 32 bits d
     adicionar_zeros_esquerda(struct_R.opcode_7,7));
     
     return binary;
-
     
 }
 char *get_I_binary(Type_I struct_I){ //Funcao que retorna o binario de 32 bits do tipo I
@@ -327,7 +247,6 @@ char *get_I_binary(Type_I struct_I){ //Funcao que retorna o binario de 32 bits d
     adicionar_zeros_esquerda(struct_I.opcode_7,7));
     
     return binary;
-
     
 }
 char *get_S_binary(Type_S struct_S){ //Funcao que retorna o binario de 32 bits do tipo S
@@ -343,7 +262,6 @@ char *get_S_binary(Type_S struct_S){ //Funcao que retorna o binario de 32 bits d
     adicionar_zeros_esquerda(struct_S.opcode_7,7));
     
     return binary;
-
     
 }
 char *get_B_binary(Type_B struct_B){ //Funcao que retorna o binario de 32 bits do tipo B
@@ -359,35 +277,15 @@ char *get_B_binary(Type_B struct_B){ //Funcao que retorna o binario de 32 bits d
     adicionar_zeros_esquerda(struct_B.opcode_7,7));
     
     return binary;
-
     
 }
-
 int get_substring(char* string,int initial_pos,int length){ //Funcao com o objetivo de pegar um intervalo(substring) dentro de uma string
     char *substring = (char*)malloc(length-initial_pos+1); //Alocacao de memoria apropriada
         strncpy(substring,string+(initial_pos-1),length); //Atribui a substring para a variavel
         return atoi(substring); //Retorna a substring como inteiro pela funcao atoi    
 }
-
-
 void set_registradores_I(Type_I * struct_I,int Rd,int immediate,int Rs1){ //Coloca os valores dos registradores e imediatos no tipo I
-    
-   
-    if (immediate < 0){
-        immediate *= -1;
-        char* str;
-        str = (char*) malloc(13*sizeof(char));
-
-        sprintf(str, "%d", immediate);   
-        size_t aux;
-        sscanf(convert_Neg_To_Bin(str), "%zu", &aux);
-        
-        struct_I->immediate_12 = aux;
-        
-        free(str);
-    }
-    
-    else struct_I->immediate_12=decimal_to_Binary(immediate); //Imediato convertido para binario
+    struct_I->immediate_12=decimal_to_Binary(immediate); //Imediato convertido para binario
     struct_I->Rs1_5=decimal_to_Binary(Rs1); //Rs1 convertido para binario
     struct_I->Rd_5=decimal_to_Binary(Rd); //Rd convertido para binario
 }
@@ -396,15 +294,13 @@ void set_registradores_R(Type_R * struct_R,int Rd,int Rs1,int Rs2){  //Coloca os
     struct_R->Rs2_5=decimal_to_Binary(Rs2); //Rs2 convertido para binario
     struct_R->Rd_5=decimal_to_Binary(Rd); //Rd convertido para binario
 }
-
 void set_registradores_S(Type_S * struct_S, int immediate,int Rs1,int Rs2){ //Coloca os valores dos registradores e imediatos no tipo S
     struct_S->immediate_7=decimal_to_Binary(get_substring(adicionar_zeros_esquerda(immediate,7),5,11)); //Imediato[11:5] convertido para binario
+    
     struct_S->Rs1_5=decimal_to_Binary(Rs1); //Rs1 convertido para binario
     struct_S->Rs2_5=decimal_to_Binary(Rs2); //Rs2 convertido para binario
     struct_S->immediate_5=decimal_to_Binary(get_substring(adicionar_zeros_esquerda(immediate,7),0,4)); //Imediato[4:0] convertido para binario 
 }
-
-
 void set_registradores_B(Type_B * struct_B, int immediate,int Rs1,int Rs2){ //Coloca os valores dos registradores e imediatos no tipo B
     struct_B->immediate_7=  decimal_to_Binary(pow(10,get_substring(adicionar_zeros_esquerda(immediate,7),12,12)) + get_substring(adicionar_zeros_esquerda(immediate,7),5,10)); //Imediato[11:5] convertido para binario
     struct_B->Rs1_5=decimal_to_Binary(Rs1); //Rs1 convertido para binario
--- program/montador.h
+++ program/montador.h
@@ -4,16 +4,10 @@
 #include <string.h>
 #include <math.h>
 #include <sys/types.h>
-
-
 #define NUM_INSTRUCT_TYPE_I 6
 #define NUM_INSTRUCT_TYPE_R 7
 #define NUM_INSTRUCT_TYPE_S 3
 #define NUM_INSTRUCT_TYPE_B 4
-
-#define NUM_CHARACTERS_MAX 50
-
-
 typedef struct Type_R { 
     /*Struct para instrucoes do tipo R
     Contem:
@@ -28,18 +22,14 @@ typedef struct Type_R {
     // Nome
     char nome_Instrucao[6];
     //Estrutura da instrução;
-    size_t funct_7; // 31 - 25
+    int funct_7; // 31 - 25
     int Rs2_5; // 24 - 20
     int Rs1_5; // 19 - 15
     int funct_3; // 14 - 12
     int Rd_5; // 11 - 7
     int opcode_7; // 6 - 0
-
     //Type of instruction "82"
-
-
 } Type_R;
-
 typedef struct Tipo_I {
        /*Struct para instrucoes do tipo I
     Contem:
@@ -52,17 +42,14 @@ typedef struct Tipo_I {
     
     //Estrutura da instrução;
     char nome_Instrucao[6];
-    size_t immediate_12; // 31 - 20
+    int immediate_12; // 31 - 20
     int Rs1_5; // 19 - 15
     int funct_3; // 14 - 12
     int Rd_5; // 11 - 7
     int opcode_7; // 6 - 0
-
     //Type of instruction "73"
     
-
 } Type_I;
-
 typedef struct TipoS {
      /*Struct para instrucoes do tipo S
     Contem:
@@ -77,17 +64,15 @@ typedef struct TipoS {
     
     //Estrutura da instrução;
     char nome_Instrucao[5];
-    size_t immediate_7; // 31 - 25
+    int immediate_7; // 31 - 25
     int Rs2_5; // 24 - 20
     int Rs1_5; // 19 - 15
     int funct_3; // 14 - 12
     int immediate_5; // 11 - 7
     int opcode_7; // 6 - 0;
-
     //Type of instruction "83"
     
 } Type_S;
-
 typedef struct TipoB {
        /*Struct para instrucoes do tipo B
     Contem:
@@ -108,11 +93,8 @@ typedef struct TipoB {
     int opcode_7; // 6 - 0
     
 } Type_B;
-
 //Funções dos Tipos
-
 /* */
-
 //Funções Primordiais
 
 char* instruct_Converter_to_Binary(char* str);
@@ -121,24 +103,23 @@ size_t octal_to_binary(size_t octal);
 size_t hexa_to_binary(char* hexa);
 size_t decimal_to_Binary(size_t decimal);
 //size_t decimal_to_Binary_neg(char* num);
-void char_To_Binary(char c, char *binario);
-void invert_Binary(char *binario);
-char* convert_Neg_To_Bin(char* c);
+
 int shift_Num(int num);
 
 //Funções de leitura de arquivo
 
 void abre_Arquivo(FILE** arqEntrada, FILE** arqSaida, char* endereco_Entrada);
 //void fecha_Arquivo(FILE arqEntrada, FILE arqSaida);
+
+short le_Linha(FILE* arqEntrada, Type_I* vetor_I, Type_R* vetor_R, Type_S* vetor_S, Type_B *vetor_B, Type_I *Result_I, Type_R *Result_R, Type_S *Result_S, Type_B *Result_B);
 void limpa_Arquivo(FILE* arq, const char* str);
 
-short le_Linha(FILE *arqEntrada,Type_I* vetor_I, Type_R* vetor_R, Type_S* vetor_S, Type_B* vetor_B, Type_I *Result_I, Type_R *Result_R, Type_S *Result_S, Type_B *Result_B); //Funcao com o objetivo de ler o arquivo de entrada linha por linha
+//MANIPULAÇÃO DOS DADOS
+short le_instrucao_R(Type_R *montador, char* str);
 short le_instrucao_S(Type_S *montador, char* str);
 short le_instrucao_I(Type_I *montador, char* str);
 short pesquisa_instrução(char* str, Type_I* montador_I, Type_R* montador_R, Type_S* montador_S, Type_B *montador_B,
 Type_I * Result_I,Type_R * Result_R,Type_S * Result_S,Type_B * Result_B);
-
-
 char* adicionar_zeros_esquerda(int binario,int num_bits);
 void set_registradores_S(Type_S * struct_S,int immediate,int Rs1,int Rs2);
 void set_registradores_R(Type_R * struct_R,int Rd,int Rs1,int Rs2);
